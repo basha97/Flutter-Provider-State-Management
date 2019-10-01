e595c345696dd4a7be0a41afc70b88e0b8432730
basha97 <bashaadil2727@gmail.com>
2019-10-01 17:56:12 +0530

    Adding the product detail data

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -7,14 +7,48 @@ class ProdcutsDetailScreen extends StatelessWidget {
   // final double price;
 
   // ProdcutsDetailScreen(this.title,this.price);
-  static const routeName  = '/product-details';
+  static const routeName = '/product-details';
   @override
   Widget build(BuildContext context) {
-    final productId   = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct = Provider.of<Products>(context,listen: false).findById(productId);
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
